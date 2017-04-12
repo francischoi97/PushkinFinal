f6bd71016ee8968711860494b38a4c6ff9ce4e73
francischoi97 <francischoi97@gmail.com>
2017-04-12 15:41:36 -0400

    Update Settings
    Made it so a useful Toast pops up when clicking on PieChart elements

--- app/src/main/java/com/pushkin/Settings.java
+++ app/src/main/java/com/pushkin/Settings.java
@@ -1,15 +1,21 @@
 package com.pushkin;
 
 import android.graphics.Color;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import java.util.ArrayList;
 
@@ -18,6 +24,7 @@ public class Settings extends AppCompatActivity {
     private String[] times={"12am-2am","2am-4am","4am-6am","6am-8am","8am-10am","10am-12pm","12pm-2pm","2pm-4pm","4pm-6pm","6pm-8pm","8pm-10pm","10pm-12am"};
     private int[] messagesbyTime={123,123,123,123,123,132,132,132,123,123,123,123};
     PieChart pieChart;
+    final String TAG="SettingsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +45,42 @@ public class Settings extends AppCompatActivity {
         int receivedNum=12345;
         int kintactsNum=1234;
 
-
-
         pieChart.setCenterText("Activity by time");
         pieChart.setRotationEnabled(true);
         pieChart.setTransparentCircleAlpha(0);
         addDataSet(pieChart);
 
+        pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                Log.d(TAG, "onValueSelected: Value selected from chart.");
+                Log.d(TAG, "onValueSelected: "+ e.toString());
+                Log.d(TAG, "onValueSelected: "+ h.toString());
+
+                String string1 = h.toString();
+                string1= string1.substring(string1.indexOf("x: ")+3);
+                string1= string1.substring(0, string1.indexOf(".0, y"));
+                Log.d(TAG, "string1: "+string1);
+                int pos=Integer.parseInt(string1);
+
+                String strings=times[pos];
+
+                int num=messagesbyTime[pos];
+
+
+                Toast.makeText(Settings.this, "Time: "+strings+"\nNumber of Interactions: "+num, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+
         header.setText("Settings");
         messagesSent.setText("Messages Sent: "+sentNum);
         messagesReceived.setText("Messages Received: "+receivedNum);
-        kintactsNumber.setText("Num ber of Kintacts: "+kintactsNum);
+        kintactsNumber.setText("Number of Kintacts: "+kintactsNum);
 
         name.setText("Joey");
         username.setText("manwhoami");
@@ -65,7 +97,7 @@ public class Settings extends AppCompatActivity {
             xEntrys.add(times[i]);
         }
 
-        PieDataSet pieDataSet= new PieDataSet(yEntrys, "Interactions");
+        PieDataSet pieDataSet= new PieDataSet(yEntrys, "Times");
         pieDataSet.setSliceSpace(2);
         pieDataSet.setValueTextSize(12);
 
